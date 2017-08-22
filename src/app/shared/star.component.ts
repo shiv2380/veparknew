5eede09165f7cb9a39acddbcb3bf1d51bdd9b38d
Shiva Shankar Prakash <shiva.shanker@kdi.kongsberg.com>
2017-08-22 14:36:35 +0530

    retained star component

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,17 +1,18 @@
-// import {Component, OnChanges, Input} from '@angular/core';
+import {Component, OnChanges, Input} from '@angular/core';
+import { SimpleChanges } from '@angular/core';
 
-// @Component({
-//     selector: 'kdi-star',
-//     templateUrl: 'star.component.html',
-//     styleUrls: ['star.component.css']
-// })
+@Component({
+    selector: 'kdi-star',
+    templateUrl: 'star.component.html',
+    styleUrls: ['star.component.css']
+})
 
-// export class StarComponent implements OnChanges {
-//     @Input() rating: number;
-//     startWidth: number;
+export class StarComponent implements OnChanges {
+    @Input() rating: number;
+    startWidth: number;
 
-//     ngOnChanges(): void {
-//         this.startWidth = this.rating * 86 / 5;
-//     }
+    ngOnChanges(changes: SimpleChanges): void {
+        this.startWidth = this.rating * 86 / 5;
+    }
 
-// }
+}
